724729747d59444a9dc991be74b057312a4e1994
Emily Nichols <emnnichols@outlook.com>
2024-08-30 11:36:24 -0700

    Created RecipeListView and RecipeDetailView

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home
+from .views import home, RecipeListView, RecipeDetailView
 
 app_name = 'recipes'
 
+## Maps the '' address to the home function-based view
 urlpatterns = [
   path('', home),
+  path('recipes/', RecipeListView.as_view(), name='list'),
+  path('recipes/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
