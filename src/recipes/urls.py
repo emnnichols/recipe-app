0229744690c14cb7cba2666f1a3c8d7dcea3552a
Emily Nichols <emnnichols@outlook.com>
2024-09-02 15:24:49 -0700

    Added authentication and login/logout functionality

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import home, RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView
 
 app_name = 'recipes'
 
 ## Maps the '' address to the home function-based view
 urlpatterns = [
-  path('', home),
   path('recipes/', RecipeListView.as_view(), name='list'),
   path('recipes/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
