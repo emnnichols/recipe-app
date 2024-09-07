e58b5a33dde3ac3222b04b3232b21b9c51463397
Emily Nichols <emnnichols@outlook.com>
2024-09-07 16:55:44 -0700

    Update urls.py

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, get_queryset
 
 app_name = 'recipes'
 
 ## Maps the '' address to the home function-based view
 urlpatterns = [
   path('recipes/', RecipeListView.as_view(), name='list'),
+  path('recipes/search', get_queryset, name='search'),
   path('recipes/<pk>', RecipeDetailView.as_view(), name='detail'),
 ]
