ad221972ad2e6a16cd4cd69ef62235790859a250
Emily Nichols <emnnichols@outlook.com>
2024-09-09 16:17:05 -0700

    Updated settings.py

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+  BASE_DIR / 'recipes/static'
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
