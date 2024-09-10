829d1ea2e2da4153ae5d92231661ae89904d806c
Emily Nichols <emnnichols@outlook.com>
2024-09-10 11:56:37 -0700

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -126,13 +126,6 @@ if DEBUG:
 else:
         STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-STORAGES = {
-        "staticfiles": {
-                "BACKEND": 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-        }
-}
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
@@ -147,3 +140,5 @@ LOGIN_URL = '/login/'
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
