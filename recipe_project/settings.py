9283e7d670847fc2e14f7a91b0686a471e908360
Emily Nichols <emnnichols@outlook.com>
2024-09-09 22:22:44 -0700

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS= [BASE_DIR /'recipes/static']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+if DEBUG:
+        STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'recipes/static')]
+else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
