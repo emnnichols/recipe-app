bde3d70a720bb1ea389aae7821214902e9104eb3
Emily Nichols <emnnichols@outlook.com>
2024-09-09 19:47:08 -0700

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-6=b-%@-)fqm-po72$lc!*x5%g5hjloqi%_l7m$^70m^zyet$81')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+  'recipe-app-python2-8b51ced6bfe8.herokuapp.com',
+  '.herokuapp.com',
+  'localhost',
+  '127.0.0.1'
+]
 
 # Application definition
 
@@ -38,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # recipe_project-related apps
-    'recipes'
+    'recipe-app',
 ]
 
 MIDDLEWARE = [
@@ -119,9 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-  BASE_DIR / 'recipes/static'
-]
+STATICFILES_DIRS= os.path.join[BASE_DIR, 'recipes/static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
@@ -137,6 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
