20ab9fe578d82b379ab999be455998dd608c32ca
Emily Nichols <emnnichols@outlook.com>
2024-09-10 12:54:27 -0700

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ BASE_DIR / 'recipes/static', BASE_DIR / 'media']
+STATICFILES_DIRS = [ BASE_DIR / 'media', 'recipes/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
