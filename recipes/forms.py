8781aa20069315a5aa0747d3815dab278e4d8341
Emily Nichols <emnnichols@outlook.com>
2024-09-10 23:43:07 -0700

    Created form for adding recipes

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Recipe
 
 CHART_CHOICES = {
   ('#1', 'Bar Chart'),
@@ -9,3 +10,11 @@ CHART_CHOICES = {
 class RecipeSearchForm(forms.Form):
   recipe_search = forms.CharField(max_length=120, label='Enter Recipe / Ingredient', required=False)
   chart_type = forms.ChoiceField(choices=CHART_CHOICES, label='Choose a Chart Type')
+
+class RecipeForm(forms.ModelForm):
+  class Meta:
+    model = Recipe
+    fields = ['name', 'cooking_time', 'ingredients', 'pic']
+    widgets = {
+      'ingredients': forms.Textarea(attrs={'rows':4, 'cols':25}),
+      }
